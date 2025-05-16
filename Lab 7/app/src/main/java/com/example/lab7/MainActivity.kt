4861f8a8380a01641db987c610b55802c1dd4fb8
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2025-05-16 14:39:16 +0300

    Kotlin lab 7

---
+++ Lab 7/app/src/main/java/com/example/lab7/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.lab7
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonFirst).setOnClickListener {
+            startActivity(Intent(this, FirstActivity::class.java))
+        }
+        findViewById<Button>(R.id.buttonSecond).setOnClickListener {
+            startActivity(Intent(this, SecondActivity::class.java))
+        }
+        findViewById<Button>(R.id.buttonThird).setOnClickListener {
+            startActivity(Intent(this, ThirdActivity::class.java))
+        }
+        findViewById<Button>(R.id.buttonExit).setOnClickListener {
+            finish()
+        }
+    }
+}
