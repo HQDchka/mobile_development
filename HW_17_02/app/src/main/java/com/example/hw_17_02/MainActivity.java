076a2a70ac77a1fa69779eae3648677d008f157b
Daniil Gaponov(Даниил Гапонов) <Daniilgaponov05@gmail.com>
2025-06-23 22:50:03 +0300

    17.02

---
+++ HW_17_02/app/src/main/java/com/example/hw_17_02/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hw_17_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button _1stActitvityButton = findViewById(R.id._1stAct);
+        Button _2ndActitvityButton = findViewById(R.id._2ndAct);
+        Button _3rdActitvityButton = findViewById(R.id._3rdAct);
+
+        _1stActitvityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        _2ndActitvityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        _3rdActitvityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
